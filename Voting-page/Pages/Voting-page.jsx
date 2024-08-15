7c88a5f3b8c6ae681dd6f6e6b68236ad6f4f12fc
LojySAzzam <lojy.Azzam@itworx.com>
2024-08-15 21:56:43 +0300

    trial

---
+++ Voting-page/Pages/Voting-page.jsx
@@ -0,0 +1,12 @@
+import styles from "./page.module.css";
+import Nominee from "./components/nominee";
+import Header from ""
+import 
+export default function Home() {
+  return (
+    <>
+      <Header/>
+    
+    </>
+  );
+}
