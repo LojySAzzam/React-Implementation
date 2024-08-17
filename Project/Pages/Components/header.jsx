3bbc49daf876532a92e894536c3f488fe61b4b07
LojySAzzam <lojy.Azzam@itworx.com>
2024-08-17 13:21:26 +0300

    Update header.jsx

--- Project/Pages/Components/header.jsx
+++ Project/Pages/Components/header.jsx
@@ -1,28 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from './header.module.css';
+import styles from './header.css';
 
 const Header = () => {
     return (
     <>
-      <div className>
+      <div className={`${styles.header}`}>
         <head>
           <meta charset="utf-8" />
           <link rel="icon" type="image/png" href={logo} />
         </head>
         <header id="mainheader">
           <nav>
-            <ul>
-              <li><img src={logo} width="75" alt="Logo" /></li>
-              <li>
+            <ul className={`${styles.ul}`}>
+              <li className={styles.li}><img src={logo} width="75" alt="Logo" /></li>
+              <li className={styles.li}>
                 <h3>Acme Inc</h3>
               </li>
-              <li><a href="homepage">Home</a></li>
+              <li className={styles.li}><a href="homepage">Home</a></li>
             </ul>
           </nav>
         </header>
       </div>
-    <>
     );
   };
 
